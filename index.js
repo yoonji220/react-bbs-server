c4233544b6253c63878bd8ebc2d6a8a544ea1f7e
choiyui <choiyj220220@gmail.com>
2026-07-02 16:38:41 +0900

    feat: implement express middleware for JSON parsing and URL encoding

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const mysql = require("mysql2");
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //json->object
+
 let corsOptions = {
   origin: "*",
 };
@@ -25,6 +28,15 @@ app.get("/list", (req, res) => {
   const sqlQuery =
     "SELECT id,title,content,writer,DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
   db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+app.post("/write", (req, res) => {
+  console.log(req.body);
+  const { title, name, content } = req.body;
+  const sqlQuery = " insert into board (title,content,writer) values (?,?,?);";
+  db.query(sqlQuery, [title, content, name], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
