c16d7427af688bf68c768164519480a41f124fe8
choiyui <choiyj220220@gmail.com>
2026-07-02 13:42:03 +0900

    feat : add cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+let corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
