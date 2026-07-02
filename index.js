176ddca7636ad08addebcbb1d1cc1a54c7472efd
choiyui <choiyj220220@gmail.com>
2026-07-02 15:03:02 +0900

    feat: replace mysql with mysql2 and implement database connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
 
 let corsOptions = {
@@ -9,8 +10,23 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "choiyj21338",
+  database: "bbs",
+});
+db.connect();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT * FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
