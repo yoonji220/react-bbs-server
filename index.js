62c00ad9f41b9736a7273de1c192ca2343e08371
choiyui <choiyj220220@gmail.com>
2026-07-03 11:45:45 +0900

    fix: correct comment for express.urlencoded and refactor /view endpoint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
 
 let corsOptions = {
   origin: "*",
@@ -28,6 +28,17 @@ app.get("/list", (req, res) => {
   const sqlQuery =
     "SELECT id,title,content,writer,DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
   db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery =
+    "SELECT title,content,writer,DATE_FORMAT(date, '%Y-%m-%d') AS date  FROM board WHERE id=?;";
+  db.query(sqlQuery, [id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
